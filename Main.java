7fc3c35b30ee7fc4594879188e0c44dc7ad34741
masset151 <48015763+masset151@users.noreply.github.com>
2020-04-27 17:08:35 +0200

    Update

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		Abonados abonado = new Abonados();
+		
+		abonado.ListarAbonados();
+		abonado.InsertarAbonado();
+		abonado.ListarAbonados();
+	
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+}
