8cdd2edee28a7a5a81ee3c1f9530db8143fcea1a
Masset <Masset@DESKTOP-162I89J>
2020-05-04 10:57:03 +0200

    añadir Sector

--- ProyectoPogramacion/src/Controlador/Main.java
+++ ProyectoPogramacion/src/Controlador/Main.java
@@ -3,12 +3,18 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+import javax.crypto.spec.SecretKeySpec;
+
 import Modelo.Abonados;
+import Modelo.Sector;
 
 public class Main {
 
 	public static void main(String[] args) throws SQLException {
 		Abonados abonado = new Abonados();
+		Sector sector = new Sector();
+		
+		sector.ListarSectores(1);
 		
 		
 		
---
+++ ProyectoPogramacion/src/Modelo/Sector.java
@@ -0,0 +1,55 @@
+package Modelo;
+
+import java.sql.ResultSet;
+
+import Controlador.ConectarBD;
+
+public class Sector {
+
+	private int codsector;
+	private String sector;
+	private int codzona;
+	
+	
+	public void ListarSectores() {
+		ConectarBD.Conectar();
+		ResultSet selectAll = ConectarBD.EjecutarSentencia("SELECT * FROM SECTOR");
+		
+		try {
+			System.out.println("codsector"+"\t"+"sector"+"\t"+"codzona");
+			while(selectAll.next()) {
+				codsector = selectAll.getInt("codsector");
+				sector = selectAll.getString("sector");
+				codzona = selectAll.getInt("codzona");
+				
+				
+				System.out.println(codsector+"\t"+sector+"\t"+codzona);
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	
+	public void ListarSectores(int num) {
+		ConectarBD.Conectar();
+		ResultSet selectAll = ConectarBD.EjecutarSentencia("SELECT * FROM SECTOR where codzona ="+num);
+		
+		try {
+			System.out.println("codsector"+"\t"+"sector"+"\t"+"codzona");
+			while(selectAll.next()) {
+				codsector = selectAll.getInt("codsector");
+				sector = selectAll.getString("sector");
+				codzona = selectAll.getInt("codzona");
+				
+				
+				System.out.println(codsector+"\t"+sector+"\t"+codzona);
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	
+	
+}
